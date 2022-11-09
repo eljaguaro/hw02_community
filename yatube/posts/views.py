48447f9c72d5a4cf40b3a6489036487d0cef8cb2
Давид Заде <Davidzade21@mail.ru>
2022-11-09 21:20:31 +0300

    commiting my new project

---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post, Group
+
+
+# Главная страница
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    template = 'posts/index.html'
+    return render(request, template, context)
+
+
+# Страница со сгруппированными постами
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts
+    }
+    template = 'posts/group_list.html'
+    return render(request, template, context)
